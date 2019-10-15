385bae6548aec5cfe01b5b4c5373a85775f00e65
Felipe Cortés <felipebrs24@hotmail.com>
2019-10-15 12:06:33 -0500

    Updates

---
+++ precache-manifest.5fba7def591bc44339332c2f31448c7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6678cece863b11ee500e2df9f7baec9f",
+    "url": "/FelipeCortes0/ProyectoWebDependencias.git/index.html"
+  },
+  {
+    "revision": "3ae1e77bcb712edd756e",
+    "url": "/FelipeCortes0/ProyectoWebDependencias.git/static/css/main.a9827aa0.chunk.css"
+  },
+  {
+    "revision": "253ad3f06b8cfe8baeed",
+    "url": "/FelipeCortes0/ProyectoWebDependencias.git/static/js/2.8136db3a.chunk.js"
+  },
+  {
+    "revision": "3ae1e77bcb712edd756e",
+    "url": "/FelipeCortes0/ProyectoWebDependencias.git/static/js/main.6f675cae.chunk.js"
+  },
+  {
+    "revision": "037d9b4a0a250956932e",
+    "url": "/FelipeCortes0/ProyectoWebDependencias.git/static/js/runtime-main.f378846d.js"
+  }
+]);
